65206f07bfbd20c9e42ddcdfc88dee0748633d10
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-03 16:58:34 +0200

    Updated LuaBot

--- source/LuaBot.lua
+++ source/LuaBot.lua
@@ -1,9 +1,11 @@
 local commands = require("../../commands")
 local botData = require("../../botData")
 local discordia = require("discordia")
-local client = discordia.Client() 
+local client = discordia.Client()
 local token = botData.Token
 local prefix = botData.Prefix
+
+local mainColor = discordia.Color.fromHex("#000080")
 
 client:on("ready", function()
 	print("Logged in as ".. client.user.username)
@@ -16,7 +18,7 @@ client:on('messageCreate', function(message)
 		local channel = message.channel
 
 		if content:sub(4,#content) == "help" then
-			commands.Help(channel)
+			commands.Help(channel, prefix, mainColor)
 		elseif content:sub(4,#content) == "ping" then
 			commands.Ping(channel)
 		end
