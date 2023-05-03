4cd06c0265f738152692b0249b32c6ef469e9bdf
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-03 16:58:57 +0200

    Updated the help command

--- source/commands.lua
+++ source/commands.lua
@@ -1,7 +1,19 @@
 local commandsModule = {}
 
-function commandsModule.Help(channel)
-	channel:send("Help")
+function commandsModule.Help(channel, prefix, embedColor)
+	channel:send {
+		embed = {
+			title = "Info",
+			description = "LuaBot is a multi-purpose Discord bot equipped with moderation tools, community managment tools and other features!",
+			fields = {
+				{name = "**Source code:**", value = "https://github.com/TenKotowsky/LuaBot", inline = false},
+				{name = "**Prefix:**", value = prefix, inline = false},
+				{name = "**General commands:**", value = "`help`, `ping`", inline = false},
+				{name = "**QOTD commands:**", value = "`addquestion`, `setqotdchannel`, `setqotdtime`, `setqotdthreadname`", inline = false}
+			},
+			color = embedColor.value
+		}
+	}
 end
 
 function commandsModule.Ping(channel)
