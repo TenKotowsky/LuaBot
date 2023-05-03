06960d2a30f0146898147f61f5f6dfb9c25bfcb4
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-03 23:05:28 +0200

    Updated commands

--- source/commands.lua
+++ source/commands.lua
@@ -9,7 +9,8 @@ function commandsModule.Help(channel, prefix, embedColor)
 				{name = "**Source code:**", value = "https://github.com/TenKotowsky/LuaBot", inline = false},
 				{name = "**Prefix:**", value = prefix, inline = false},
 				{name = "**General commands:**", value = "`help`, `ping`", inline = false},
-				{name = "**QOTD commands:**", value = "`addquestion`, `setqotdchannel`, `setqotdtime`, `setqotdthreadname`", inline = false}
+				{name = "**QOTD commands (upcoming):**", value = "`addquestion`, `setqotdchannel`, `setqotdtime`, `setqotdthreadname`", inline = false},
+				{name = "**4Fun commands:**", value = "`robloxuserinfo [username]`", inline = false}
 			},
 			color = embedColor.value
 		}
@@ -18,6 +19,28 @@ end
 
 function commandsModule.Ping(channel)
 	channel:send("Pong!")
+end
+
+function commandsModule.RobloxUserInfo(channel, embedColor, data, finalThumbnailData)
+	print(finalThumbnailData.imageUrl)
+	if data and finalThumbnailData then
+		local verifiedMsg = "Not verified"
+		if data.hasVerifiedBadge then
+			verifiedMsg = "Verified"
+		end
+		channel:send {
+			embed = {
+				title = "Roblox user info",
+				description = data.displayName.." ("..data.name..")",
+				thumbnail = {url = finalThumbnailData.imageUrl},
+				fields = {
+					{name = "Verification status:", value = verifiedMsg, inline = false},
+					{name = "User ID:", value = data.id, inline = false}
+				},
+				color = embedColor.value
+			}
+		}
+	end
 end
 
 return commandsModule
