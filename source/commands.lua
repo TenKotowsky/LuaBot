86cef3d7875c01071d48f2cb51278000916fa095
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-05 15:47:18 +0200

    Updated commands

--- source/commands.lua
+++ source/commands.lua
@@ -11,7 +11,7 @@ function commandsModule.Help(channel, prefix, embedColor)
 				{name = "**Prefix:**", value = prefix, inline = false},
 				{name = "**General commands:**", value = "`help`, `ping`", inline = false},
 				{name = "**Moderation commands:**", value = "`ban [user]`, `kick [user]`, `slowmode [0-21600s]`", inline = false},
-				{name = "**4Fun commands:**", value = "`quote`, `robloxuser [username]`", inline = false}
+				{name = "**4Fun commands:**", value = "`quote`, `randomnumber [min] [max]`, `robloxuser [username]`", inline = false}
 			},
 			color = embedColor.value
 		}
@@ -36,6 +36,33 @@ local quotes = {
 function commandsModule.RandomQuote(channel)
 	channel:send(quotes[math.random(1,#quotes)])
 end
+
+function commandsModule.RandomNumber(message, argument1, argument2)
+	local number1
+	if tonumber(argument1) then
+		number1 = tonumber(argument1)
+		if number1 > 1000000000000 then
+			number1 = 1000000000000
+		end
+	else
+		message:reply("Enter a valid minimum number!")
+		return
+	end
+
+	local number2
+	if tonumber(argument2) then
+		number2 = tonumber(argument2)
+		if number2 > 1000000000000 then
+			number2 = 1000000000000
+		end
+	else
+		message:reply("Enter a valid maximum number!")
+		return
+	end
+
+	message:reply(math.random(number1, number2))
+end
+
 
 function commandsModule.RobloxUser(channel, embedColor, data, finalThumbnailData)
 	if data and finalThumbnailData then
