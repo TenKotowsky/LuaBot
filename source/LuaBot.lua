57a097e2f2feb843d8b153f12907f979b1f7ab2f
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-08 14:20:39 +0200

    Update LuaBot.lua

--- source/LuaBot.lua
+++ source/LuaBot.lua
@@ -4,7 +4,8 @@ local discordia = require("discordia")
 local coroutine = require("coroutine")
 local client = discordia.Client()
 local token = botData.Token
-local prefix = botData.Prefix
+local sqlite3 = require("../../deps/sqlite3")
+local conn = sqlite3.open("DataBase.sqlite")
 
 local mainColor = discordia.Color.fromHex("#000080")
 
@@ -27,29 +28,40 @@ end
 client:on('messageCreate', function(message)
 	local content = message.content
 	local channel = message.channel
+	local guildId = tonumber(channel.guild.id)
+	local prefix
+	local prefixData = conn:exec("SELECT prefix FROM serverconfig WHERE guildId = '"..guildId.."';")
+	if prefixData == nil then
+		prefix = botData.Prefix
+	else
+		prefix = prefixData.prefix[1]
+	end
+
 	if content:sub(1,#prefix) == prefix then
 
-		if content:sub(4,#content) == "help" then
+		if content:sub(#prefix+1,#content) == "help" then
 			commands.Help(channel, prefix, mainColor)
-		elseif content:sub(4,#content) == "ping" then
+		elseif content:sub(#prefix+1,#content) == "ping" then
 			commands.Ping(channel)
-		elseif content:sub(4,#content) == "quote" then
+		elseif content:sub(#prefix+1,#content) == "quote" then
 			commands.RandomQuote(channel)
 		else
 			local words = splitMessage(content)
-			if words[1]:sub(4,#words[1]) == "robloxuser" and words[2] then
+			if words[1]:sub(#prefix+1,#words[1]) == "robloxuser" and words[2] then
 				commands.RobloxUser(channel, mainColor, words)
-			elseif words[1]:sub(4,#words[1]) == "ban" then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "ban" then
 				commands.Ban(message)
-			elseif words[1]:sub(4,#words[1]) == "kick" then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "kick" then
 				commands.Kick(message)
-			elseif words[1]:sub(4,#words[1]) == "slowmode" then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "slowmode" then
 				commands.Slowmode(message, words)
-			elseif words[1]:sub(4,#words[1]) == "userinfo" and words[2] then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "prefix" and words[2] then
+				commands.Prefix(message, channel.guild.id, words[2])
+			elseif words[1]:sub(#prefix+1,#words[1]) == "userinfo" and words[2] then
 				commands.UserInfo(message.guild, channel, mainColor, message.mentionedUsers.first)
-			elseif words[1]:sub(4,#words[1]) == "avatar" and words[2] then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "avatar" and words[2] then
 				commands.Avatar(channel, mainColor, message.mentionedUsers.first)
-			elseif words[1]:sub(4,#words[1]) == "spotifyartist" and words[2] then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "spotifyartist" and words[2] then
 				local searchedArtist = ""
 				local selectedWords = {}
 				for i, v in ipairs(words) do
@@ -65,7 +77,7 @@ client:on('messageCreate', function(message)
 					end
 				end
 				commands.SpotifyArtist(message, mainColor, searchedArtist, botData.SpotifyClientId, botData.SpotifySecret)
-			elseif words[1]:sub(4,#words[1]) == "spotifyalbum" and words[2] then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "spotifyalbum" and words[2] then
 				local searchedAlbum = ""
 				local selectedWords = {}
 				for i, v in ipairs(words) do
@@ -81,7 +93,7 @@ client:on('messageCreate', function(message)
 					end
 				end
 				commands.SpotifyAlbum(message, mainColor, searchedAlbum, botData.SpotifyClientId, botData.SpotifySecret)
-			elseif words[1]:sub(4,#words[1]) == "randomnumber" and words[2] and words[3] then
+			elseif words[1]:sub(#prefix+1,#words[1]) == "randomnumber" and words[2] and words[3] then
 				commands.RandomNumber(message, words[2], words[3])
 			end
 		end
