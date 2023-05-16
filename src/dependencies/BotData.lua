7b015b9fc933cf295c820f8b47d70ae3a1b74b14
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-16 19:26:39 +0200

    Add files via upload

---
+++ src/dependencies/BotData.lua
@@ -0,0 +1,8 @@
+local botData = {}
+
+botData.Token = "Bot MTEwNTEyODUwNjk5MzY4MDUwNw.GEBAmF.3lAZOFWzGooeY3gbj7ZUqTi75SKSJ9b-rnZoNY"
+botData.Prefix = "lua"
+botData.SpotifyClientId = "180ed3f40f094180a6b37376004a6828"
+botData.SpotifySecret = "5e1e461197a54a129a5d4878b4878493"
+
+return botData
