17cfbb9ccc526fe2196d2e8720ff6d86e4cccffa
TenKotowsky <100159503+TenKotowsky@users.noreply.github.com>
2023-05-19 22:02:52 +0200

    Update BotData.lua

--- src/dependencies/BotData.lua
+++ src/dependencies/BotData.lua
@@ -2,7 +2,7 @@ local botData = {}
 
 botData.Token = "Bot ..."
 botData.Prefix = "lua"
-botData.SpotifyClientId = "180ed3f40f094180a6b37376004a6828"
-botData.SpotifySecret = "5e1e461197a54a129a5d4878b4878493"
+botData.SpotifyClientId = "..."
+botData.SpotifySecret = "..."
 
 return botData
